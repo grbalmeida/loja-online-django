fbcc461d37291b47aa8193fc9230897dbeaf0a9f
grbalmeida <g.r.almeida@live.com>
2022-11-27 18:03:34 -0300

    Estendendo o site de administração com views personalizadas

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -2,6 +2,8 @@ import csv
 import datetime
 from django.http import HttpResponse
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from .models import Order, OrderItem
 
 class OrderItemInline(admin.TabularInline):
@@ -32,11 +34,19 @@ def export_to_csv(modeladmin, request, queryset):
     return response
 export_to_csv.short_description = 'Export to CSV'
 
+# Evite usar mark_safe em uma entrada fornecida por usuário a fim
+# de evitar o XSS (Cross-Site Scripting, ou Scripting Entre Sites). O XSS
+# permite que invasores injetem scripts do lado do cliente em conteúdos
+# web visualizados por outros usuários
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
-from .models import OrderItem
+from django.contrib.admin.views.decorators import staff_member_required
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from .tasks import order_created
 from cart.cart import Cart
@@ -35,3 +36,11 @@ def order_create(request):
         form = OrderCreateForm()
 
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+# O decorador staff_member_required verifica se ambos os campos, is_active
+# e is_staff, do usuário que está requisitando a página estão definidos como True.
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
