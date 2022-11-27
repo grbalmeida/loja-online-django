8cba8c0d202457a6f32778763c7ab104ca7cc6e9
grbalmeida <g.r.almeida@live.com>
2022-11-27 14:10:48 -0300

    Adicionando ações personalizadas no site de administração

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,9 +1,36 @@
+import csv
+import datetime
+from django.http import HttpResponse
 from django.contrib import admin
 from .models import Order, OrderItem
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+
+    fields = [field for field in opts.get_fields() if not \
+    field.many_to_many and not field.one_to_many]
+
+    # Escreve uma primeira linha com informações de cabeçalho
+    writer.writerow([field.verbose_name for field in fields])
+    # Escreve as linhas de dados
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -12,3 +39,4 @@ class OrderAdmin(admin.ModelAdmin):
                     'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
