a73d6717b267b5fe20a17c08475028427bcffd27
grbalmeida <g.r.almeida@live.com>
2022-12-02 15:16:24 -0300

    Renderizando arquivos PDF

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -151,3 +151,5 @@ BRAINTREE_CONF = braintree.Configuration(
     BRAINTREE_PUBLIC_KEY,
     BRAINTREE_PRIVATE_KEY
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -42,6 +42,11 @@ def order_detail(obj):
     url = reverse('orders:admin_order_detail', args=[obj.id])
     return mark_safe(f'<a href="{url}">View</a>')
 
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+order_pdf.short_description = 'Invoice'
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
