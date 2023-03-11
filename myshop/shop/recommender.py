b988909b64ce52fd59c2636d85fb0602cb35d9da
grbalmeida <g.r.almeida@live.com>
2023-03-10 23:39:08 -0300

    Adicionando método que limpa as recomendações

--- myshop/shop/recommender.py
+++ myshop/shop/recommender.py
@@ -56,3 +56,7 @@ class Recommender(object):
         suggested_products.sort(key=lambda x: suggested_products_ids.index(x.id))
         
         return suggested_products
+    
+    def clear_purchases(self):
+        for id in Product.objects.values_list('id', flat=True):
+            r.delete(self.get_product_key(id))
