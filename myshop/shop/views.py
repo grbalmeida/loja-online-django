188c8da7d8c0565a3a4331c4dff261458e7c8fad
grbalmeida <g.r.almeida@live.com>
2023-03-11 00:05:22 -0300

    Exibindo produtos recomendados na página de detalhe do produto

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from cart.forms import CartAddProductForm
 from .models import Category, Product
+from .recommender import Recommender
 
 def product_list(request, category_slug=None):
     category = None
@@ -28,5 +29,12 @@ def product_detail(request, id, slug):
         available=True)
     
     cart_product_form = CartAddProductForm()
+    
+    r = Recommender()
+    recommended_products = r.suggest_products_for([product], 4)
 
-    return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product,
+                   'cart_product_form': cart_product_form,
+                   'recommended_products': recommended_products})
