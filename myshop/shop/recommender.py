da4f09b1384bb9d5fefc969bceb857844941dab6
grbalmeida <g.r.almeida@live.com>
2023-03-10 23:37:49 -0300

    Adicionando método que recomenda produtos para comprar junto

--- myshop/shop/recommender.py
+++ myshop/shop/recommender.py
@@ -22,3 +22,37 @@ class Recommender(object):
                 if product_id != with_id:
                     # incrementa a pontuação dos produtos comprados juntos
                     r.zincrby(self.get_product_key(product_id), 1, with_id)
+                    
+    def suggest_products_for(self, products, max_results=6):
+        product_ids = [p.id for p in products]
+        
+        if len(products) == 1:
+            # somente 1 produto
+            suggestions = r.zrange(self.get_product_key(product_ids[0]),
+                                   0, -1, desc=True)[:max_results]
+        else:
+            # gera uma chave temporária
+            flat_ids = ''.join([str(id) for id in product_ids])
+            tmp_key = f'tmp_{flat_ids}'
+            
+            # vários produtos; combina as pontuações de todos os produtos,
+            # armazena o conjunto ordenado resultante em uma chave temporária
+            keys = [self.get_product_key(id) for id in product_ids]
+            r.zunionstore(tmp_key, keys)
+            
+            # remove os ids dos produtos para os quais a recomendação será feita
+            r.zrem(tmp_key, *product_ids)
+            
+            # obtém os ids dos produtos em ordem decrescente de suas pontuações
+            suggestions = r.zrange(tmp_key, 0, -1, desc=True)[:max_results]
+            
+            # remove a chave temporária
+            r.delete(tmp_key)
+
+        suggested_products_ids = [int(id) for id in suggestions]
+        
+        # obtém os produtos sugeridos e ordena conforme a ordem em que aparecem
+        suggested_products = list(Product.objects.filter(id__in=suggested_products_ids))
+        suggested_products.sort(key=lambda x: suggested_products_ids.index(x.id))
+        
+        return suggested_products
